45355c195b787a42e58b5e55e829a5f6e185e5ed
Itai Wiseman <wmndev@gmail.com>
2016-06-25 01:10:05 -0400

    Dishes + orderNow pages

--- static/js/angular/app.js
+++ static/js/angular/app.js
@@ -46,31 +46,46 @@ app.controller('mainController', ['$scope', '$uibModal', '$log', function($scope
 
 
 app.controller('dishesController', ['$scope', '$log', function($scope, $log){
-    var dishesContent = [{name: 'My first dish name',
-                         shortDescription: 'my f short description'},
-                        {name: 'My second dish name',
-                         shortDescription: 'my s short description'},
+    var dishesContent = [{name: 'Chraime',
+                         shortDescription: 'Traditional Moroccan fish dish, It contains peppers, cilantro, and tomatoes and lots of Moroccan spices',
+                         description: 'This dish is a tribute to Shula, mother of my sister in law and a great cook. I wish I could make all of the Moroccan dishes she make but that would require a lifetime of practice and going back in time to far places. Lucky for me Charime is easy to make as long as you have the right spices. It is spicy, it is tangy it Charimelicious!'},
+                        {name: '4 hours meatballs',
+                         shortDescription: 'What is the short description here?',
+                        description: 'This one is inspired by two recipes that came my way. Martha Stewart’s meat balls with rosemary and lemon zest and Frankie’s four hours tomato sauce. I use different mix of meat every time depends of the mood, the weather and the map of the stars. It is truly a homey and hearty and comforting food.'},
                         {name: 'My thrid dish name',
-                         shortDescription: 'my t short description'},
+                         shortDescription: 'my t short description',
+                        description: 'itay number three'},
                         {name: 'My fourth dish name',
-                         shortDescription: 'my fourth short description'}]
+                         shortDescription: 'my fourth short description',
+                        description: 'itay number four'}]
 
     $scope.dishes = {
         one:{
             name: dishesContent[0].name,
-            shortDescription: dishesContent[0].shortDescription
+            shortDescription: dishesContent[0].shortDescription,
+            description: dishesContent[0].description,
+            idx:'1'
+
         },
         two:{
             name: dishesContent[1].name,
-            shortDescription: dishesContent[1].shortDescription
+            shortDescription: dishesContent[1].shortDescription,
+            description: dishesContent[1].description,
+            idx:'2'
+
         } ,
         three:{
             name: dishesContent[2].name,
-            shortDescription: dishesContent[2].shortDescription
+            shortDescription: dishesContent[2].shortDescription,
+            description: dishesContent[2].description,
+            idx:'3'
+
         },
         four:{
             name: dishesContent[3].name,
-            shortDescription: dishesContent[3].shortDescription
+            shortDescription: dishesContent[3].shortDescription,
+            description: dishesContent[3].description,
+            idx:'4'
         }
     }  
     
